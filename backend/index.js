d7914cbbf59e42deb1e426233062af61d6f8c854
davidtzz <david.tovarz@udea.edu.co>
2024-09-30 11:35:41 -0500

    Update 11:35 am

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const session = require('express-session'); 
-const sequelize = require('./config/db');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/db'); // Tu conexión a MySQL
 const User = require('./models/userModel');  
 const authRoutes = require('./routes/authRoutes');  
+const cors = require('cors');
 
 const app = express();
 
-const cors = require('cors');
+// CORS setup
 app.use(cors({
   origin: 'https://main.d3gn7cununfdbc.amplifyapp.com', // Cambia esto si es necesario
   credentials: true, // Permite enviar cookies
@@ -16,21 +18,36 @@ app.use(cors({
 // Middleware para procesar JSON
 app.use(express.json());
 
-// Configuración de express-session
+// Configuración de express-session con SequelizeStore
+const sessionStore = new SequelizeStore({
+  db: sequelize, // Conexión a la base de datos
+});
+
 app.use(session({
   secret: 'dftz09122003', // Cambia esto por un valor seguro
-  cookie: {secure: true}
+  store: sessionStore,
+  resave: false, // Evita guardar la sesión si no ha sido modificada
+  saveUninitialized: false, // No guarda sesiones vacías
+  cookie: {
+    secure: true, // Solo en HTTPS
+    httpOnly: true,
+    sameSite: 'none',
+  }
 }));
 
-// Usa las rutas de autenticación
-app.use('/api/auth', authRoutes);  
+// Sincronizar la base de datos para la tabla de sesiones
+sessionStore.sync();
 
-// Sincroniza el modelo con la base de datos
+// Usa las rutas de autenticación
+app.use('/api/auth', authRoutes);
+
+// Sincronizar los modelos
 sequelize.sync()
   .then(() => console.log('La base de datos y las tablas han sido sincronizadas'))
   .catch(err => console.error('Error al sincronizar la base de datos:', err));
 
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-  });
+// Iniciar el servidor
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
