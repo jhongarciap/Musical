80153696240ee17ae6b1bb3e190204b8aa245ba6
jhongarciap <jhon.garciap@udea.edu.co>
2024-10-01 19:37:41 -0500

    link

--- frontend/my-app/src/components/Header_home.jsx
+++ frontend/my-app/src/components/Header_home.jsx
@@ -6,7 +6,7 @@ function Header() {
 
   const handleLogout = async () => {
     try {
-      const response = await axios.post('https://backmusical.onrender.com/api/auh/logout');
+      const response = await axios.post('https://backmusical.onrender.com/api/auth/logout');
       console.log(response);
       if (response.status === 200) {
         // Redirige a la página principal después de cerrar sesión
