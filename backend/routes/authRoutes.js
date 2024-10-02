af9467bbd699670f10233c13779a4a9f7186bc4e
jhongarciap <jhon.garciap@udea.edu.co>
2024-10-01 22:46:06 -0500

    prueba 2

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,9 +9,11 @@ router.get('/lastfm', redirectToLastFm);
 // Ruta para el callback de Last.fm
 router.get('/callback', lastFmCallback);
 
-// Ruta para obtener la información del usuario desde la sesión
+// Ruta para obtener el perfil del usuario
 router.get('/profile', async (req, res) => {
   try {
+    console.log('Sesión actual:', req.session);  // Verifica la sesión
+
     // Verificar si la sesión contiene un usuario
     if (req.session && req.session.username) {
       // Buscar al usuario en la base de datos utilizando Sequelize
@@ -19,26 +21,25 @@ router.get('/profile', async (req, res) => {
       
       // Verificar si el usuario fue encontrado
       if (user) {
-        console.log('Usuario encontrado en base de datos:', user);
+        console.log('Usuario encontrado en la base de datos:', user);  // Verifica los datos del usuario
 
-        // Responder con la información del usuario
+        // Devolver los datos del usuario
         return res.status(200).json({
           username: user.username,
           profile_image: user.profile_image,
           is_pro: user.is_pro,
         });
       } else {
-        // Si no se encontró al usuario, devolver 404
+        console.warn('No se encontró el usuario en la base de datos.');
         return res.status(404).json({ message: 'Usuario no encontrado' });
       }
     } else {
-      // Si no hay sesión o no se ha iniciado sesión
+      console.warn('No se encontró una sesión activa o el campo username está ausente en la sesión.');
       return res.status(401).json({ message: 'No hay sesión activa o no se ha iniciado sesión' });
     }
   } catch (error) {
-    // Manejo de errores
-    console.error('Error al obtener el perfil del usuario:', error);
-    return res.status(500).json({ message: 'Error del servidor' });
+    console.error('Error al obtener el perfil del usuario:', error);  // Detalle del error
+    return res.status(500).json({ message: 'Error del servidor', error: error.message });
   }
 });
 
