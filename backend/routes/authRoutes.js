7d1478bf5a54c1e17e31eaf9d7dc7d3488a6838b
jhongarciap <jhon.garciap@udea.edu.co>
2024-10-02 00:20:06 -0500

    sesion

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,20 +9,32 @@ router.get('/lastfm', redirectToLastFm);
 // Ruta para el callback de Last.fm
 router.get('/callback', lastFmCallback);
 
-// Ruta para obtener la información del usuario
 router.get('/profile', async (req, res) => {
   try {
+    // Verificamos si la sesión contiene un usuario válido
     if (req.session && req.session.username) {
-      const user = await Users.findOne({ where: { username } } );
+      console.log('Usuario en la sesión:', req.session.username); // Verifica que exista
+
+      // Consulta a la base de datos
+      const user = await Users.findOne({ where: { username: req.session.username } });
+
       if (user) {
+        console.log('Usuario encontrado:', user);
+
+        // Devuelve los datos del usuario encontrados
         return res.status(200).json({
           username: user.username,
           profile_image: user.profile_image,
           is_pro: user.is_pro,
         });
+      } else {
+        console.log('No se encontró al usuario');
+        return res.status(404).json({ message: 'Usuario no encontrado' });
       }
+    } else {
+      console.log('No hay usuario en la sesión');
+      return res.status(401).json({ message: 'No hay usuario autenticado en la sesión' });
     }
-    return res.status(404).json({ message: 'Usuario no encontrado' });
   } catch (error) {
     console.error('Error al obtener el perfil del usuario:', error);
     return res.status(500).json({ message: 'Error del servidor' });
