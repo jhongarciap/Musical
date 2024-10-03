4ba81fa46b13cfdacb13307aa5ad50e4fd1f22d2
jhongarciap <jhon.garciap@udea.edu.co>
2024-10-02 23:22:46 -0500

    link front

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,7 +58,7 @@ const lastFmCallback = async (req, res) => {
       { expiresIn: '1h' } // Tiempo de expiración del token
     );
 
-  res.redirect(`https://main.d3swbnx2em39af.amplifyapp.com/callback?token=${jwtToken}`);
+  res.redirect(`https://salmon-sea-0b585031e.5.azurestaticapps.net/callback?token=${jwtToken}`);
   } catch (error) {
     console.error('Error al obtener la sesión:', error);
     res.status(500).send('Error durante la autenticación');
